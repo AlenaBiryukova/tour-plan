462098eb9356f797f7b92be20b7ee8d58eb217e1
AlenaBiryukova <biraleona@yandex.ru>
2021-11-25 09:54:16 +0300

    fix: added google map, changed ico

--- js/main.js
+++ js/main.js
@@ -13,17 +13,3 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     onlyInViewport: false,
   },
 });
-
-var myMap;
-ymaps.ready(init);
-function init () {
-    myMap = new ymaps.Map('map', {
-        center: [25.078276, 55.134926],
-        zoom: 17
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
-    document.getElementById('destroyButton').onclick = function () {
-        myMap.destroy();
-    };
-}
