601a2882aa8650e7d282f243d79fb1edd732251a
AlenaBiryukova <biraleona@yandex.ru>
2021-12-02 14:17:45 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -30,3 +30,11 @@ $('.newsletter').parallax({
   imageSrc: 'img/newsletter-bg.jpeg',
   speed: 0.3,
 });
+
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function () {
+  console.log('Клик по кнопке меню');
+  document
+    .querySelector('.navbar-button')
+    .classList.toggle('navbar-button--visible');
+});
